0ea41450a4651028241c0f5a8961383bc0b5642d
Vinicius Shinokawa <viniciusakutagawa24@gmail.com>
2024-06-14 16:18:03 -0300

    Adicionar arquivos de projeto.

---
+++ DescontoDeProduto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DescontoDeProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco;
+            double desconto;
+
+            Console.WriteLine("Informe o valor do produto"); 
+            preco = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            if (preco >= 200) desconto = preco * 10 / 100;
+            else
+                desconto = preco * 12.5 / 100; 
+
+
+            Console.WriteLine("Valor do produto = " +preco+ " desconto = " +desconto);       
+            Console.WriteLine("Valor a pagar = "+(preco - desconto));
+            Console.ReadKey(); 
+
+
+
+        }
+    }
+}
---
+++ DescontoDeProduto/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("DescontoDeProduto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DescontoDeProduto")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("fe2b9cf9-f337-4dde-bd7b-77a3d7de7297")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
